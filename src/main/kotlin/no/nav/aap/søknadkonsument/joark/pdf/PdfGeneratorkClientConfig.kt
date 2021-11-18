b8c83a6d6978aba14b509b2bbd7b28e2ddb8fad9
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-18 18:42:07 +0100

    size

--- src/main/kotlin/no/nav/aap/søknadkonsument/joark/pdf/PdfGeneratorkClientConfig.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/joark/pdf/PdfGeneratorkClientConfig.kt
@@ -8,11 +8,14 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.http.codec.ServerCodecConfigurer
+import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient
 
+
 @Configuration
-class PdfGeneratorkClientConfig  {
+class PdfGeneratorkClientConfig :WebFluxConfigurer {
     @Qualifier(PDFGEN)
     @Bean
     fun webClientPdfGen(builder: WebClient.Builder, cfg: PDFGeneratorConfig, env: Environment): WebClient {
@@ -21,5 +24,9 @@ class PdfGeneratorkClientConfig  {
             .baseUrl(cfg.baseUri.toString())
             .filter(correlatingFilterFunction())
             .build()
+    }
+
+    override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
+        configurer.defaultCodecs().maxInMemorySize(50 * 1024 * 1024)
     }
 }
