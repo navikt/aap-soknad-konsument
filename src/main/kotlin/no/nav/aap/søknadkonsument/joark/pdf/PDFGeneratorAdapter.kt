719dbc6e17b000b3e37dca64cfee300fd908c432
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-18 17:33:20 +0100

    error handling

--- src/main/kotlin/no/nav/aap/søknadkonsument/joark/pdf/PDFGeneratorAdapter.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/joark/pdf/PDFGeneratorAdapter.kt
@@ -5,8 +5,10 @@ import no.nav.aap.søknadkonsument.config.Constants.PDFGEN
 import no.nav.aap.søknadkonsument.rest.AbstractWebClientAdapter
 import no.nav.aap.søknadkonsument.util.LoggerUtil
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 
@@ -15,12 +17,13 @@ class PDFGeneratorAdapter(@Qualifier(PDFGEN) client: WebClient, val cf: PDFGener
     private val log = LoggerUtil.getLogger(javaClass)
 
     fun  generate(søknad: UtenlandsSøknadKafka) : ByteArray {
-        log.debug("Creating PDF from $søknad.")
+        log.debug("Creating PDF from $søknad via ")
         return webClient.post()
             .uri { it.path(cf.path).build() }
             .contentType(APPLICATION_JSON)
             .bodyValue(søknad)
             .retrieve()
+            .onStatus({ obj: HttpStatus -> obj.isError }) { obj: ClientResponse -> obj.createException() }
             .bodyToMono<ByteArray>()
             .block() ?: throw RuntimeException("PDF could not be generated")
 
