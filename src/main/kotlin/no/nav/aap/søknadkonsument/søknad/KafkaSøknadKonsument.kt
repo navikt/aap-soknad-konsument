45ef835bf8b20dabb20a08a7ad35a209275d76f4
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-23 13:58:51 +0100

    lag journalpost ordentlig

--- src/main/kotlin/no/nav/aap/søknadkonsument/søknad/KafkaSøknadKonsument.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/søknad/KafkaSøknadKonsument.kt
@@ -16,26 +16,22 @@ import java.util.*
 class KafkaSøknadKonsument(val joark: JoarkClient, val pdfGen: PDFGeneratorClient) {
     private val log = LoggerUtil.getLogger(javaClass)
 
-    @KafkaListener(topics = ["#{'\${utenlands.topic:aap.aap-utland-soknad-sendt.v1}'}"],  groupId = "#{'\${spring.kafka.consumer.group-id}'}")
+    @KafkaListener(, topics = ["#{'\${utenlands.topic:aap.aap-utland-soknad-sendt.v1}'}"],  groupId = "#{'\${spring.kafka.consumer.group-id}'}")
     fun konsumer(consumerRecord: ConsumerRecord<String, UtenlandsSøknadKafka>,@Header(NAV_CALL_ID)  callId: String) {
         MDCUtil.toMDC(NAV_CALL_ID,callId)
         val søknad = consumerRecord.value()
         val fnr = consumerRecord.key();
         log.trace("WOHOO, fikk søknad $søknad")
-        val id = joark.opprettJournalpost(Journalpost(tilleggsopplysninger = tillegg(), dokumenter = docs(søknad),tema = "AAP", tittel="jalla", avsenderMottaker = AvsenderMottaker(fnr,navn="Gurba"), bruker = Bruker(fnr)))
+        val id = joark.opprettJournalpost(Journalpost(tilleggsopplysninger = listOf(), dokumenter = docs(søknad),tema = "AAP", tittel="Søknad om å beholde AAP ved opphold i utlandet", avsenderMottaker = AvsenderMottaker(fnr,navn="Gurba"), bruker = Bruker(fnr)))
         log.info("WOHOO, fikk arkivert $id")
     }
 
-    private fun tillegg(): List<Tilleggsopplysning> {
-       return listOf(Tilleggsopplysning("a","b"))
-    }
-
     private fun docs(søknad: UtenlandsSøknadKafka): List<Dokument> {
-        return listOf(Dokument("a","b", listOf(
+        return listOf(Dokument("Søknad om å beholde AAP ved opphold i utlandet","NAV 11-03.07", listOf(
             DokumentVariant(
-                "a",
+                "PDF/A",
                 Base64.getEncoder().encodeToString(pdfGen.generate(søknad)),
-                "c"
+                "Arkiv"
             ))))
     }
 }
