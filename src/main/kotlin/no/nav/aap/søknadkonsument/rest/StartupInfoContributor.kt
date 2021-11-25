98e0ee4e909f8681ca892b4acc33b9e12b9c24bb
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-11-25 11:18:20 +0100

    startup time

--- src/main/kotlin/no/nav/aap/søknadkonsument/rest/StartupInfoContributor.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/rest/StartupInfoContributor.kt
@@ -3,18 +3,16 @@ import org.springframework.boot.actuate.info.Info
 import org.springframework.boot.actuate.info.InfoContributor
 import org.springframework.stereotype.Component
 import java.lang.management.ManagementFactory.getRuntimeMXBean
-import java.time.Instant
-import java.time.ZoneId
-import java.util.Map
+import java.time.Instant.ofEpochMilli
+import java.time.ZoneId.systemDefault
 
 @Component
 class StartupInfoContributor : InfoContributor {
     override fun contribute(builder: Info.Builder) {
         builder.withDetail(
-            "nais", Map.of(
-                "Startup time",
-                Instant.ofEpochMilli(getRuntimeMXBean().startTime).atZone(ZoneId.systemDefault())
-                    .toLocalDateTime(),
+            "nais", mapOf(
+                "Startup time" to
+                ofEpochMilli(getRuntimeMXBean().startTime).atZone(systemDefault()).toLocalDateTime(),
             )
         )
     }
