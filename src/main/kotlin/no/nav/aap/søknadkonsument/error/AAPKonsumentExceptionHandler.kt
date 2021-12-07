c9f715e091abf8b007f721a820295892f2da25a5
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-07 10:53:10 +0100

    error handling

--- src/main/kotlin/no/nav/aap/søknadkonsument/error/AAPKonsumentExceptionHandler.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/error/AAPKonsumentExceptionHandler.kt
@@ -7,9 +7,15 @@ import org.springframework.core.env.Environment
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.WebClientResponseException.BadRequest
+import org.springframework.web.reactive.function.client.WebClientResponseException.Forbidden
+import org.springframework.web.reactive.function.client.WebClientResponseException.NotFound
+import org.springframework.web.reactive.function.client.WebClientResponseException.Unauthorized
 import org.zalando.problem.Problem
+import org.zalando.problem.Status.BAD_REQUEST
+import org.zalando.problem.Status.INTERNAL_SERVER_ERROR
 import org.zalando.problem.Status.NOT_FOUND
 import org.zalando.problem.Status.UNAUTHORIZED
 import org.zalando.problem.spring.web.advice.ProblemHandling
@@ -22,9 +28,15 @@ class AAPKonsumentExceptionHandler(val authContext: AuthContext, private val env
     fun handleMissingOrExpiredToken(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
         create(UNAUTHORIZED, e, req)
 
-    @ExceptionHandler(HttpClientErrorException.NotFound::class)
-    fun handleNotFound(e: java.lang.Exception, req: NativeWebRequest): ResponseEntity<Problem> =
-        create(NOT_FOUND, e, req)
+    @ExceptionHandler(WebClientResponseException::class)
+    fun handleWebClientResponseException(e: WebClientResponseException, req: NativeWebRequest): ResponseEntity<Problem> {
+        return when (e) {
+            is BadRequest -> create(BAD_REQUEST,e,req)
+            is Forbidden, is Unauthorized -> create(UNAUTHORIZED,e,req)
+            is NotFound -> create(NOT_FOUND,e,req)
+            else -> create(INTERNAL_SERVER_ERROR,e,req)
+        }
+    }
 
     override fun toString() = "${javaClass.simpleName} [authContext=$authContext]"
 }
