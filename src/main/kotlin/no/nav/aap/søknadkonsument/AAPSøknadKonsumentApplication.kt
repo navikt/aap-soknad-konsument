9ca96119ff2264c72cbcfab0c28ae6d01a8a0dc4
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-12-19 15:43:44 +0100

    main

--- src/main/kotlin/no/nav/aap/søknadkonsument/AAPSøknadKonsumentApplication.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/AAPSøknadKonsumentApplication.kt
@@ -5,6 +5,7 @@ import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.retry.annotation.EnableRetry
@@ -17,13 +18,10 @@ import org.springframework.retry.annotation.EnableRetry
 @EnableRetry
 @EnableKafka
  class  AAPSøknadKonsumentApplication {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplicationBuilder(AAPSøknadKonsumentApplication::class.java)
-                .profiles(*profiler())
-                .main(AAPSøknadKonsumentApplication::class.java)
-                .run(*args)
-        }
-     }
+    fun main(args: Array<String>) {
+        SpringApplicationBuilder(AAPSøknadKonsumentApplication::class.java)
+            .profiles(*profiler())
+            .applicationStartup(BufferingApplicationStartup(4096))
+            .main(AAPSøknadKonsumentApplication::class.java)
+            .run(*args)
  }
