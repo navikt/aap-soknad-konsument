886cccf30aaf389fc0279b79a90c6fa84ad0672f
Jan-Olav Eide <jan.olav.eide@gmail.com>
2022-04-07 11:15:55 +0200

    log

--- src/main/kotlin/no/nav/aap/søknadkonsument/joark/JoarkClientConfig.kt
+++ src/main/kotlin/no/nav/aap/søknadkonsument/joark/JoarkClientConfig.kt
@@ -1,22 +1,17 @@
 package no.nav.aap.søknadkonsument.joark
 
 import no.nav.aap.health.AbstractPingableHealthIndicator
-import no.nav.aap.rest.AbstractWebClientAdapter.Companion.correlatingFilterFunction
 import no.nav.aap.rest.AbstractWebClientAdapter.Companion.temaFilterFunction
 import no.nav.aap.søknadkonsument.rest.AADFilterFunction
 import no.nav.aap.util.Constants.JOARK
-import no.nav.boot.conditionals.EnvUtil.isDevOrLocal
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
-import reactor.netty.http.client.HttpClient
 
 @Configuration
-class JoarkClientConfig(@Value("\${spring.application.name}") val applicationName: String) {
+class JoarkClientConfig {
 
     @Qualifier(JOARK)
     @Bean
@@ -25,9 +20,7 @@ class JoarkClientConfig(@Value("\${spring.application.name}") val applicationNam
                        aadFilterFunction: AADFilterFunction,
                        env: Environment) =
         builder
-            .clientConnector(ReactorClientHttpConnector(HttpClient.create().wiretap(isDevOrLocal(env))))
             .baseUrl(cfg.baseUri.toString())
-            .filter(correlatingFilterFunction(applicationName))
             .filter(temaFilterFunction())
             .filter(aadFilterFunction)
             .build()
